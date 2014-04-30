ea73c750527aac104bd8ec93087c20f0e5ffb24d
Jong S. Lee <jongsang76@gmail.com>
2014-04-30 00:06:27 -0400

    finished Web App

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
 
 BLACKJACK_AMOUNT = 21
 DEALER_MIN_HIT = 17
+INITIAL_POT_AMOUNT = 10000
 
 helpers do 
   def calculate_total(cards) # card is [["H","3"], ["D", "J"], ...]
@@ -53,19 +55,21 @@ helpers do
   def winner!(msg)
     @play_again=true
      @show_hit_or_stay_buttons = false
-     @success = "<strong>#{session[:player_name]} wins! </strong> #{msg}"
+     session[:player_pot] = session[:player_pot] + session[:player_bet]
+     @winner = "<strong>#{session[:player_name]} wins! </strong> #{msg}"
   end 
   
   def loser!(msg)
     @play_again=true
      @show_hit_or_stay_buttons = false
-    @error ="<strong> #{session[:player_name]} loses. </strong> #{msg}" 
+      session[:player_pot] = session[:player_pot] - session[:player_bet]
+    @loser ="<strong> #{session[:player_name]} loses. </strong> #{msg}" 
   end  
 
   def tie!(msg)
     @play_again=true
      @show_hit_or_stay_buttons = false
-    @success ="<strong> It's a tie!</strong>#{msg}"
+    @winner ="<strong> It's a tie!</strong>#{msg}"
   end
 end
 
@@ -83,6 +87,7 @@ get '/' do
 end
 
 get '/new_player' do
+  session[:player_pot]=INITIAL_POT_AMOUNT
   erb :new_player
 end
 
@@ -94,7 +99,25 @@ post '/new_player' do
 
   session[:player_name] = params[:player_name]
   #progress to the game
-  redirect '/game'
+  redirect '/bet'
+end
+
+get '/bet' do
+  session[:player_bet] = nil
+  erb :bet
+end
+
+post '/bet' do
+  if params[:bet_amount].nil? || params[:bet_amount].to_i ==0
+      @error= "Must make a bet"
+      halt erb(:erb)
+  elsif params[:bet_amount].to_i > session[:player_pot]
+    @error = "Bet amount cannot be greater than what you have ($#{session[:player_pot]})"
+    halt erb(:erb)
+  else ##happy path
+    session[:player_bet] = params[:bet_amount].to_i
+    redirect '/game'
+  end
 end
 
 get '/game' do
@@ -130,7 +153,7 @@ post '/game/player/hit' do
       loser! ("It looks like #{session[:player_name]} busted at #{player_total}.")
   end 
 
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/player/stay' do
@@ -158,7 +181,7 @@ get '/game/dealer' do
     @show_dealer_hit_button = true
   end 
 
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/dealer/hit' do
@@ -185,7 +208,7 @@ get '/game/compare' do
     tie!(" Both #{session[:player_name]} and the dealer stayed at #{player_total}.")
   end
 
-  erb :game
+  erb :game, layout: false
 end
 
 get '/game_over' do 
